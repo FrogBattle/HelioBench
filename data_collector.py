03b7f00c62989395f59102ee6252066546114e2d
Alexander Simeonov <alexander.sim@yahoo.com>
2021-12-09 18:03:18 +0000

    Monitoring module progress
    - Write a data_collector script to recursively fetch, adapting to errors
    - Document all relevant queries we would need from Prometheus.
    - Add promscale implementation into the Go benchmark

---
+++ data_collector.py
@@ -0,0 +1,54 @@
+import requests
+from time import time
+import csv
+
+PROMETHEUS_URL = 'http://localhost:9090/api/v1/query_range'
+ONE_SECOND = 1
+ONE_MINUTE_IN_SECONDS = 60*ONE_SECOND
+ONE_HOUR_IN_SECONDS = 60 * ONE_MINUTE_IN_SECONDS
+ONE_DAY_IN_SECONDS = 24 * ONE_HOUR_IN_SECONDS
+
+def collect_metrics(query, start_time = time() - ONE_DAY_IN_SECONDS, end_time = time(), step = 15):
+    query = "avg by (instance) (irate(node_cpu_seconds_total{mode='system'}[1m])) * 100"
+
+    if start_time >= end_time:
+        return []
+    
+    response = attempt_step(query, start_time, end_time, step)
+
+    if(response['status'] == 'success'):
+
+        if(len(response['data']['result']) == 0):
+            return []
+
+        return response['data']['result'][0]['values']
+    else:
+        if response['errorType'] == 'bad_data' and 'exceeded maximum resolution' in response['error']:
+            middle_time = start_time + (end_time - start_time) / 2
+            res_part_1 = collect_metrics(query, start_time, middle_time, step)
+            res_part_1.extend(collect_metrics(query, middle_time, end_time, step))
+            print("Breaking down")
+            return res_part_1
+        else: 
+            print(f"Error collecting data for query: {query}", response)
+
+def attempt_step(query, start, end, step):
+    response = requests.get(PROMETHEUS_URL, params = {"query":query, "start": start, "end":end, "step":step}).json()
+    return response
+
+
+if __name__ == '__main__':
+    query = "avg by (instance) (irate(node_cpu_seconds_total{mode='system'}[1m])) * 100"
+    res = collect_metrics(query)
+    print(f'Collected {len(res)} total datapoints for query:{query}')
+    title = ['query', query]
+    header = ['epoch', 'result']
+
+    print("saving to file")
+    with open('result.csv', 'w') as f:
+        writer = csv.writer(f)
+
+        writer.writerow(title)
+        writer.writerow(header)
+
+        writer.writerows(res)
