a343cca1fbe5c7c6ceb160beb8658b090dd96f51
Alexander Simeonov <alexander.sim@yahoo.com>
2022-01-03 21:07:49 +0000

    HOTFIX(Services): Integrate latest orchestrator into all benchmarks
    Background: Because of all the changes in #1, actions need to be taken to ensure benchmarks are working and comply to the new environment variables functionality.
    
    Actions:
    
    - For each service:
     - Abstract out a target port in the experiment config file and inject it into the docker-compose image via envvars.
     - Add a new network `HelioBench-network` to ensure communciation between a benchmark and the unified Prometheus server
     - Ensure the workload works with the specified experiment config.
     - Remove any traces of per-service Prometheus architecture
    
    - For older benchmarks (alexnet/text-analysis)
     - Add a Dockerfile with Node explorer and startup scripts whenever needed.
    
    See #1

--- microservices/go-api/go-api/startup_script.sh
+++ microservices/go-api/go-api/startup_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ./node_exporter-1.2.2.linux-amd64 && ./node_exporter &
+cd ./node_exporter-1.2.2.linux-amd64 && ./node_exporter --web.listen-address=":$PROMETHEUS_TARGET_PORT" &
 ./bin/main
 
 # Wait for any process to exit
